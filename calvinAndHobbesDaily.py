096699fbf38a1ea2441c2f89c2bd95f5389d17aa
Sreesh Mallya <sreeshsmallya@gmail.com>
2017-05-18 17:07:04 +0530

    Changed `filename` + added `comic`

--- calvinAndHobbesDaily.py
+++ calvinAndHobbesDaily.py
@@ -1,6 +1,8 @@
 """
 Downloads the Calvin and Hobbes comic strip for the day from www.gocomics.com.
 Simply copy this script into a folder and run it where you want the comic strip to be downloaded. :)
+
+Requires package beautifulsoup4. Install using command: pip install beautifulsoup4
 """
 
 from bs4 import BeautifulSoup
@@ -11,17 +13,16 @@ user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10
 headers = {'User-Agent': user_agent}
 
 comic = 'calvinandhobbes'
-base_url = 'http://www.gocomics.com/' + comic + '/'   # Can be modified to URL of any comic on www.gocomics.com
-
-today = datetime.datetime.now().strftime("%Y/%m/%d")
+dt = datetime.datetime.now().strftime("%Y/%m/%d")
+url = 'http://www.gocomics.com/' + comic + '/' + dt   # Can be modified to URL of any comic on www.gocomics.com
 
 filename = comic + '-' + datetime.datetime.now().strftime("%Y-%m-%d") + '.gif'
 
-req = Request(base_url+today, None, headers)
+req = Request(url, None, headers)
 
 with urlopen(req) as response:
     html = response.read()
 
-soup = BeautifulSoup(html, 'lxml')
+soup = BeautifulSoup(html, 'http.parser')
 img_src = soup.find_all('picture', {'class': 'img-fluid item-comic-image'})[0].img['src']
 urlretrieve(img_src, filename)  # Save file with `filename`
