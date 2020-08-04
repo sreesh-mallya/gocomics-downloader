12ee91f868f0751d1c9791f8278e83e4465bb856
Sreesh Mallya <sreeshsmallya@gmail.com>
2020-08-04 12:16:14 +0530

    Added setup.py and some command line args

--- gocomicsd/gocomicsd.py
+++ gocomicsd/gocomicsd.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import datetime
 
@@ -12,6 +13,15 @@ LOGGER = logging.getLogger('gocomicsd')
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0'
 HEADERS = {'User-Agent': USER_AGENT}
 BASE_URL = r'http://www.gocomics.com/'
+
+
+def parse_args(cl_args):
+    parser = argparse.ArgumentParser(description='Download comic strips from GoComics.')
+    parser.add_argument('--path', type=str, help='Path to save downloaded files to.')
+    parser.add_argument('--from', type=str, help='Download comic strips from date DD-MM-YYYY.')
+    parser.add_argument('--to', type=str, help='Download comic strips from date DD-MM-YYYY.')
+    parser.add_argument('--name', type=str, help='Comic to download. Use `--list` to view available comics.')
+    parser.add_argument('--list', type=str, help='View available comics.')
 
 
 def download(comic: str, date: str) -> None:
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gocomics-downloader',
+    version='0.0.1',
+    packages=['gocomicsd'],
+    url='',
+    license='',
+    author='Sreesh Mallya',
+    author_email='sreeshsmallya@gmail.com',
+    description='Download comic strips from gocomics.com'
+)
